c1e448c16f9c851dbe02f49e44cb0046f1685422
Giorgia Colucci <giorgia.uni23@gmail.com>
2026-07-03 13:20:45 +0200

    Pulizia finale

--- Esercitazioni/esercitazione_08/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pcsdocker/Data/Esercitazioni_PCS_2026/esercitazione8
-# Build directory: /home/pcsdocker/Data/Esercitazioni_PCS_2026/esercitazione8/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[test_grafi]=] "/home/pcsdocker/Data/Esercitazioni_PCS_2026/esercitazione8/build/main")
-set_tests_properties([=[test_grafi]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/pcsdocker/Data/Esercitazioni_PCS_2026/esercitazione8/CMakeLists.txt;10;add_test;/home/pcsdocker/Data/Esercitazioni_PCS_2026/esercitazione8/CMakeLists.txt;0;")
--- Esercitazioni/esercitazione_10/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pcsdocker/Data/Esercitazioni_PCS_2026/esercitazione10
-# Build directory: /home/pcsdocker/Data/Esercitazioni_PCS_2026/esercitazione10/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[test_gradiente]=] "/home/pcsdocker/Data/Esercitazioni_PCS_2026/esercitazione10/build/main")
-set_tests_properties([=[test_gradiente]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/pcsdocker/Data/Esercitazioni_PCS_2026/esercitazione10/CMakeLists.txt;9;add_test;/home/pcsdocker/Data/Esercitazioni_PCS_2026/esercitazione10/CMakeLists.txt;0;")
